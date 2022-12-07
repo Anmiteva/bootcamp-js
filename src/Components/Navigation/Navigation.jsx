9b4dbeaa0851fb105276e2b452bd90461199990e
stoilova85 <42897519+stoilova85@users.noreply.github.com>
2022-12-07 19:15:49 +0200

    Branch nasko (#1)
    * react router and navigation implementation
    
    * react router and navigation implementation correction on Home link
    
    Co-authored-by: Atanas Kitov <atanaskitov@gmail.com>

---
+++ src/Components/Navigation/Navigation.jsx
@@ -0,0 +1,34 @@
+import { Link } from "react-router-dom"
+
+
+const Navigation = () => {
+    return <header>
+        <div className="logo">
+            <img src="./logo_elixir.png" alt="logo" />
+        </div>
+        <div className="navigation">
+            <ul>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/about">About</Link>
+                </li>
+                <li>
+                    <Link to="/menu">Menu</Link>
+                </li>
+                <li>
+                    <Link to="/gallery">Gallery</Link>
+                </li>
+                <li>
+                    <Link to="/reservation">Reservation</Link>
+                </li>
+                <li>
+                    <Link to="/location">Location</Link>
+                </li>
+            </ul>
+        </div>
+    </header>
+}
+
+export default Navigation
