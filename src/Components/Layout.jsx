a6da361bc469ba302db81da8cad44f0c8acb6a6a
stoilova85 <42897519+stoilova85@users.noreply.github.com>
2023-01-27 19:17:56 +0200

    Update Layout.jsx
    import menu in layout

--- src/Components/Layout.jsx
+++ src/Components/Layout.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router";
 import React from "react";
 import Home from "./Home";
 import About from "./About/About";
+import Menu from "./Menu/Menu";
 
 const Layout = () => {
 
